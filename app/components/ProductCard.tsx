0908c52ed8bc4ac08ae56401630a38bef2e62a0e
voideus <bikash.tamang@wesionary.team>
2023-11-27 13:29:45 +0545

    feat: Integrate daisy ui

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -3,7 +3,7 @@ import AddToCard from "./AddToCart";
 
 const ProductCard = () => {
   return (
-    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600">
+    <div>
       <AddToCard />
     </div>
   );
