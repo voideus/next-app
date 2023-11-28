a713def89759e87faf686687aea154c62b7373ff
voideus <bikash.tamang@wesionary.team>
2023-11-28 13:25:47 +0545

    feat: Add google auth provider

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,13 +3,14 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-5 space-x-3">
       <Link href="/" className="mr-5">
         Next.js
       </Link>
       <Link href="/users" className="mr-5">
         Users
       </Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
