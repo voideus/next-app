707935766b53e98c6e38cba0a922afb77cc77ba3
voideus <bikash.tamang@wesionary.team>
2023-11-28 11:44:58 +0545

    feat: Add products model

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import { prisma } from "@/prisma/client";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Milk", price: 2.5 },
-    { id: 2, name: "Bread", price: 1.5 },
-  ]);
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -19,8 +18,12 @@ export async function POST(request: NextRequest) {
     );
   }
 
-  return NextResponse.json(
-    { id: 10, name: body.name, price: body.price },
-    { status: 201 }
-  );
+  const product = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(product, { status: 201 });
 }
