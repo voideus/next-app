9f7e69a34a87e9360e4612dc8e0d7558e75de42e
voideus <bikash.tamang@wesionary.team>
2023-11-28 15:13:30 +0545

    chore:protected route

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -15,7 +15,14 @@ const NavBar = () => {
         Users
       </Link>
 
-      {status === "authenticated" && <div>{session.user!.name}</div>}
+      {status === "authenticated" && (
+        <div>
+          {session.user!.name}
+          <Link href="/api/auth/signout" className="ml-3">
+            Sign Out
+          </Link>
+        </div>
+      )}
       {status === "unauthenticated" && (
         <Link href="/api/auth/signin">Login</Link>
       )}
---
+++ middleware.ts
@@ -0,0 +1,17 @@
+// import { NextRequest, NextResponse } from "next/server";
+// import middleware from "next-auth/middleware";
+export { default } from "next-auth/middleware";
+
+// manual
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/new-page", request.url));
+// }
+
+// export default middleware;
+
+export const config = {
+  // *: zero or more
+  // +: one or more
+  // ?: zero or one
+  //   matcher: ["/users/:id*"],
+};
