5eadb98d5cdb0d2e9a95192fdf4133d3169cf129
voideus <bikash.tamang@wesionary.team>
2023-11-29 11:29:51 +0545

    feat: Register user

--- middleware.ts
+++ middleware.ts
@@ -9,9 +9,10 @@ export { default } from "next-auth/middleware";
 
 // export default middleware;
 
-// export const config = {
-//   // *: zero or more
-//   // +: one or more
-//   // ?: zero or one
-//   //   matcher: ["/users/:id*"],
-// };
+export const config = {
+  // *: zero or more
+  // +: one or more
+  // ?: zero or one
+  //   matcher: ["/users/:id*"],
+  matcher: ["/dashboard/:path*"],
+};
